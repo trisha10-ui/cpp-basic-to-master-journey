7e4589e0200ff51dfd2b6c9a375c23e9002ff6e2
TRISHA ANIL THEPALE <trishathepale1016@gmail.com>
2026-05-18 21:50:55 +0530

    Create char_array.cpp
    Char arr[] stores the string value in an array.

---
+++ basics/char_array.cpp
@@ -0,0 +1,8 @@
+//Write a C++ Program to declare a character array,initialize it with the string "Welcome to the C++ Programming", and display its contents on the console.
+#include<iostream>
+using namespace std;
+int main(){
+	char arr[]="Welcome to the C++ Programming";
+	cout<<"This is the value of array: " <<arr<<endl;
+	return 0;
+}
