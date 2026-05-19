4e0826b319971bf597074de05d028bf5e899ad0b
TRISHA ANIL THEPALE <trishathepale1016@gmail.com>
2026-05-19 20:26:00 +0530

    area of circle program in C++
    To calculate the area of a circle
    Formula: area = pi * r * r
    We used float because the area in the manual is always in decimal. 
    And there is no option of square directly in codes, so we used r*r.

---
+++ basics/area_of_circle.cpp
@@ -0,0 +1,12 @@
+//Write a C++ program to Calculate the area of circle , area=pi*r^2
+#include<iostream>
+using namespace std;
+int main(){
+  float r=5.6;
+float a=3.14*r*r;
+cout<<"The Area of circle is: " <<a;
+  return 0;
+  }
+
+
+//output: The Area of circle is: 98.4704
