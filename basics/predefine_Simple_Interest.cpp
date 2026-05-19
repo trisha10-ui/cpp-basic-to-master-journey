a55d5e27f5f1da9971e6ebbc31e4197ef79730aa
TRISHA ANIL THEPALE <trishathepale1016@gmail.com>
2026-05-19 20:36:19 +0530

    Create predefine_Simple_Interest.cpp
    To calculate simple interest (predefined)
    The formula is SI=P*R*T/100.00.
    
    We use float because this time R (rate of interest) is a float, and to avoid typecasting, we mention all the variables in float.

---
+++ basics/predefine_Simple_Interest.cpp
@@ -0,0 +1,14 @@
+//Write a cpp program to Calculate Simple interest (pre define)
+#include<iostream>
+using namespace std;
+int main(){
+	float P=1000;
+	float R=0.4;
+	float T=36;
+	float SI=P*R*T/100.00;
+	cout<<"The SI is: "<<SI;
+	return 0;
+}
+
+
+//Output: The SI is: 144
