ff6d08a1bb61278c60bf8dc6b131f2ace49e378c
TRISHA ANIL THEPALE <trishathepale1016@gmail.com>
2026-05-20 17:07:08 +0530

    Add multiplication_of_two_no.cpp program
    Implement a C++ program to multiply two numbers.

---
+++ basics/multiplication_of_two_no.cpp
@@ -0,0 +1,14 @@
+//Write a Cpp program to multiply the two nos.
+#include<iostream>
+using namespace std;
+int main(){
+	int x,y;
+	cout<<"Enter two no. x and y : "<<endl;
+	cin>>x>>y;
+	cout<<"The multiplication of two no is: "<<x*y;
+	return 0;
+}
+
+//output:Enter two no. x and y :
+//5 9
+//The multiplication of two no is: 45
