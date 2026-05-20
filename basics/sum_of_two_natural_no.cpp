f360d6dcf29c3a85e876be22109b580686597349
TRISHA ANIL THEPALE <trishathepale1016@gmail.com>
2026-05-20 17:08:26 +0530

    Add program to sum two natural numbers
    This program prompts the user to enter two natural numbers and outputs their sum.

---
+++ basics/sum_of_two_natural_no.cpp
@@ -0,0 +1,16 @@
+//Write a cpp program to addition(sum) of two natural numbers
+#include<iostream>
+using namespace std;
+int main(){
+	int x,y;
+	cout<<"Enter the first no: ";
+	cin>>x;
+	cout<<"Enter the second no: ";
+	cin>>y;
+	cout<<"The Sum of two no is: "<<x+y;
+	return 0;
+}
+//output:
+//Enter the first no: 5
+//Enter the second no: 9
+//The Sum of two no is: 14
