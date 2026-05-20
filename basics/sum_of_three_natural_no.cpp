943968c0c2c10197f4f4ba28e7fa32f4d208dbd3
TRISHA ANIL THEPALE <trishathepale1016@gmail.com>
2026-05-20 17:08:57 +0530

    Add program to sum three natural numbers
    This program takes three natural numbers as input from the user and calculates their sum.

---
+++ basics/sum_of_three_natural_no.cpp
@@ -0,0 +1,14 @@
+//Write a cpp program to take input from user for 3 natural nos
+#include<iostream>
+using namespace std;
+int main(){
+	int n1,n2,n3;
+	cout<<"Enter the 3 nos. n1, n2, n3: "<<endl;
+	cin>>n1>>n2>>n3;
+	cout<<"Sum of 3 no is: "<<n1+n2+n3;
+	return 0;
+}
+
+//Output:Enter the 3 nos. n1, n2, n3:
+//7 8 9
+//Sum of 3 no is: 24
