3f54bee2701370b706a57f929552f44c88c81bdb
TRISHA ANIL THEPALE <trishathepale1016@gmail.com>
2026-05-20 17:07:38 +0530

    Create square_of_a_no.cpp

---
+++ basics/square_of_a_no.cpp
@@ -0,0 +1,16 @@
+//Write a cpp program to take value from user to take out square of a no.
+//user define(major use of cin)
+//square of no
+#include<iostream>
+using namespace std;
+int main(){
+	int x;
+	cout<<"Enter the value of x: ";
+	cin>>x;
+	cout<<"Square of a no is: "<<x*x;
+	return 0;
+}
+
+//output:Enter the value of x: 5
+//Square of a no is: 25
+
