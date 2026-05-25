142daa9712b3865e9a14f73e36cb072fc918251a
TRISHA ANIL THEPALE <trishathepale1016@gmail.com>
2026-05-25 21:25:47 +0530

    Create hierachy_of_operations_Que2.cpp

---
+++ Practice_Question_based_on_Hierachy_of_operations/hierachy_of_operations_Que2.cpp
@@ -0,0 +1,16 @@
+//Practice Problems
+//Hierachy of operators 
+//Que 2 Predict The Output
+#include<iostream>
+using namespace std;
+int main(){
+	int i=2,j=3,k,l;
+	float a,b;
+	k=i/j*j;
+	l=j/i*i;
+	a=i/j*j;
+	b=j/i*i;
+	cout<<"k:"<<k<<" , l:"<<l<<" , a:"<<a<<" , b:"<<b;
+}
+
+//output:k:0 , l:2 , a:0 , b:2
