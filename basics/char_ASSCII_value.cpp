4a1f81cbdfc2b8d7e52e434256c0f3d8c49155c1
TRISHA ANIL THEPALE <trishathepale1016@gmail.com>
2026-05-25 20:22:51 +0530

    Add program to display ASCII value of a character
    This program takes a character input from the user and displays its corresponding ASCII value.

---
+++ basics/char_ASSCII_value.cpp
@@ -0,0 +1,20 @@
+//Write a cpp program to take user input for a character and display its ASCII Value.
+#include<iostream>
+using namespace std;
+int main(){
+	char ch;
+	cout<<"Enter Ch value: "<<endl;
+	cin>>ch;
+	cout<<(int)ch; //this prints the character value
+}
+
+//output:
+//Enter Ch value:
+//a
+//97
+
+
+//Enter Ch value:
+//h
+//104
+
