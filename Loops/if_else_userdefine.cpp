616090a6a6299ca0a3791debc61cbd0afd73b804
TRISHA ANIL THEPALE <trishathepale1016@gmail.com>
2026-05-25 20:34:39 +0530

    Add even or odd checker program
    Implement a program to check if a number is even or odd based on user input.

---
+++ Loops/if_else_userdefine.cpp
@@ -0,0 +1,21 @@
+//if_else  //userdefine
+//Write a cpp program which is userdefine variable and prints the Statement 
+//the Number/value is even or odd. 
+#include<iostream>
+using namespace std;
+int main(){
+	int x;
+	cout<<"Enter the value of x: ";
+	cin>>x;
+	if(x%2==0)
+	cout<<"Value is even";
+	else
+	cout<<"value is odd";
+}
+
+//output:  Enter the value of x:
+//8
+//Value is even
+//
+//Enter the value of x: 3
+//value is odd
