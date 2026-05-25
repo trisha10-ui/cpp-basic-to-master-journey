dd9b717debfebb3a276be4689047adb10e9af8b9
TRISHA ANIL THEPALE <trishathepale1016@gmail.com>
2026-05-25 20:37:58 +0530

    Add if loop example for checking even numbers
    Implement if loop to check if a number is even using a predefined variable.

---
+++ Loops/if_loop_predefine.cpp
@@ -0,0 +1,15 @@
+//Loops
+//if loop Statement  //predefined
+//Write a cpp program to execute if condition true using predefined variable.
+#include<iostream>
+using namespace std;
+int main(){
+	int num=10;
+	if(num % 2==0)
+	{
+		cout<<"Number is even ";
+	}
+	
+}
+
+//output: Number is even  //as num=10 remender is 0 (%) modulus operator
