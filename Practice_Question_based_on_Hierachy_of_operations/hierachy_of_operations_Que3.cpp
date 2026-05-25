c1e93d4286b61eb8e778e2f1fad845d55477c9ca
TRISHA ANIL THEPALE <trishathepale1016@gmail.com>
2026-05-25 21:26:29 +0530

    Implement hierarchy of operations question in C++
    Added a C++ program to demonstrate operator hierarchy with a question about operation precedence.

---
+++ Practice_Question_based_on_Hierachy_of_operations/hierachy_of_operations_Que3.cpp
@@ -0,0 +1,19 @@
+//Practice Problems
+//Hierachy of operators 
+//Que 3 Predict The Output
+//int b=6.6 / a+2*n; which operation will be performed first?
+//A-6.6/  (Corrected Answer) use Left to Right Associativity rule
+//B- a+2
+//C-2*n
+//D-Depend on the compiler
+
+
+//Implementing in program 
+#include<iostream>
+using namespace std;
+int main(){
+	int b=6.6 / a+2*n;  //int n,a; a and b can be any value based on the user input we can use cin 
+	cout<<"value of b is: "<<b;
+}
+
+//hence the Question asked for Which operation will be performed first so 6.6 will performed first.
