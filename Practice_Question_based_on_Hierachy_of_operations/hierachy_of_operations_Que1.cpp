38ae2504797df9cc6258f16408918781b508dca3
TRISHA ANIL THEPALE <trishathepale1016@gmail.com>
2026-05-25 21:25:11 +0530

    Add C++ program for operator hierarchy question
    Implement a C++ program to evaluate a predefined math equation using operator hierarchy.

---
+++ Practice_Question_based_on_Hierachy_of_operations/hierachy_of_operations_Que1.cpp
@@ -0,0 +1,13 @@
+//Practice Problems
+//Hierachy of operators 
+//Que 1 : Write a cpp program which have predefine maths equation int i=2*3/4+4/4+8-2+5/8
+//Note before implementing in Programs try manually it will better to understand the questions.
+#include<iostream>
+using namespace std;
+int main(){
+	int i=2* 3/4 + 4/4 + 8-2 + 5/8;
+	cout<<"The value of i is: ";
+	cout<<i;
+}
+
+//Output: The value of i is: 8
