8028927bd32caa15d6b8921c22ae78843e8d130a
TRISHA ANIL THEPALE <trishathepale1016@gmail.com>
2026-06-04 18:48:46 +0530

    Add program to print numbers from 1 to 100
    Implement a for loop to print numbers from 1 to 100.

---
+++ Loop/For Loop/for_loop_1_to_100_print.cpp
@@ -0,0 +1,12 @@
+//for loop
+//Write a program to print number from 1 to 100 using for loop.
+
+#include<iostream>
+using namespace std;
+int main(){
+	int i;
+	for(i=1; i<=100;i++)
+	{
+		cout<<i<<" ";
+	}
+}
