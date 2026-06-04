ac9c1f9d1583176b066f268e864b449878456ac8
TRISHA ANIL THEPALE <trishathepale1016@gmail.com>
2026-06-04 18:49:41 +0530

    Add program to print even numbers from 1 to 100

---
+++ Loop/For Loop/for_loop_even_print_1_to_100_print.cpp
@@ -0,0 +1,13 @@
+//for loop
+//Write program to print all even numbers from 1 to 100 using for loop.
+#include<iostream>
+using namespace std;
+int main(){
+	for(int i=1;i<=100;i++)
+	{
+		if(i%2==0)
+		{
+			cout<<i<<" ";
+		}
+	}
+} 
