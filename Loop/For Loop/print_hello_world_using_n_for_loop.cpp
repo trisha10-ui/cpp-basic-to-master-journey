d4ce761bb49a44deabaa60dccb1c40f5d2794755
TRISHA ANIL THEPALE <trishathepale1016@gmail.com>
2026-06-04 18:50:57 +0530

    Add program to print 'Hello world' n times
    This program prompts the user for a number and prints 'Hello world' that many times.

---
+++ Loop/For Loop/print_hello_world_using_n_for_loop.cpp
@@ -0,0 +1,13 @@
+//Write a program to print hello world n time using user input
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"Enter n: ";
+	cin>>n;
+	
+	for(int i=1;i<=n;i++)
+	{
+		cout<<"Hello world"<<endl;
+	}
+}
