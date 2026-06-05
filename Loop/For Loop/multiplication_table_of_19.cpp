a7d1bcee5f95fce1c19c99bfe30a59ee20ba7bf2
TRISHA ANIL THEPALE <trishathepale1016@gmail.com>
2026-06-05 11:47:16 +0530

    Add multiplication table program for 19
    This program displays the multiplication table of 19 from 1 to 10.

---
+++ Loop/For Loop/multiplication_table_of_19.cpp
@@ -0,0 +1,11 @@
+//Write a program to display the table of 19
+#include<iostream>
+using namespace std;
+int main(){
+	int num=19;
+	for(int i=1;i<=10;i++)
+	{
+		cout<<num<<" X "<<i<<" = "<<num*i<<endl;
+	}
+	return 0;
+}
