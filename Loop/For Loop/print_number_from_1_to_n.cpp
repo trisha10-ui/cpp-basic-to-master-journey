e12585386ac0232e4d6ecdc4b2ee2fbce50cf63c
TRISHA ANIL THEPALE <trishathepale1016@gmail.com>
2026-06-11 21:15:52 +0530

    Add program to print numbers from 1 to n

---
+++ Loop/For Loop/print_number_from_1_to_n.cpp
@@ -0,0 +1,12 @@
+//Write a program to print a number from 1 to n.
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"Enter n value: ";
+	cin>>n;
+	for(int i=1;i<=n;i++)
+	{
+		cout<<i<<endl;
+	}
+}
