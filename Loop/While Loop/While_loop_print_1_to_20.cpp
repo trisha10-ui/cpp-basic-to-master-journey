36ae945eb37498d39a1b001c27f85fb5304ae296
TRISHA ANIL THEPALE <trishathepale1016@gmail.com>
2026-06-18 20:58:54 +0530

    Create While_loop_print_1_to_20.cpp

---
+++ Loop/While Loop/While_loop_print_1_to_20.cpp
@@ -0,0 +1,14 @@
+//Write a program to print no from 1 to 20 Using while loop
+#include<iostream>
+using namespace std;
+int main(){
+	int i=1;
+	while(i<=20)
+	{
+		cout<<i<<" ";
+		i++;
+	}
+	return 0;
+}
+
+//output: 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
