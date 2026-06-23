ec757638d9a15edd9edb6aa5faa25491d7158182
TRISHA ANIL THEPALE <trishathepale1016@gmail.com>
2026-06-23 19:51:06 +0530

    Create number_reverse triangle.cpp
    //Enter n: 4
    //       1
    //     1 2
    //   1 2 3
    // 1 2 3 4

---
+++ Pattern Printing/number_reverse triangle.cpp
@@ -0,0 +1,27 @@
+//Enter n: 4
+//       1
+//     1 2
+//   1 2 3
+// 1 2 3 4
+
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"Enter n: ";
+	cin>>n;
+	
+	for(int i=1; i<=n; i++){
+		for(int j=1;j<=n-i; j++)
+		{
+			cout<<"  ";
+		}
+			for(int j=1; j<=i; j++)
+			{
+				cout<<" "<<j;
+			}
+		
+		cout<<endl;
+	}
+	return 0;
+}
