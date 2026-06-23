5f0146817fc6f6c1ae52a768afd37a280d5acf6a
TRISHA ANIL THEPALE <trishathepale1016@gmail.com>
2026-06-23 19:50:12 +0530

    Create star_reverse_triangle.cpp
    ////Enter n: 4
    //      *
    //    * *
    //  * * *
    //* * * *

---
+++ Pattern Printing/star_reverse_triangle.cpp
@@ -0,0 +1,25 @@
+//star reverse reverse triangle
+////Enter n: 4
+//      *
+//    * *
+//  * * *
+//* * * *
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"Enter n: ";
+	cin>>n;
+	
+	for(int i=1;i<=n;i++){
+		for(int j=1; j<=n-i;j++){
+			cout<<"  ";
+		}
+		for(int j=1; j<=i; j++)
+		{
+			cout<<"* ";
+		}
+		cout<<endl;
+
+	}
+}
