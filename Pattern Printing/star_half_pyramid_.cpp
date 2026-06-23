a3cf53a22ba9c5f4fee7bb171af99654e19519b5
TRISHA ANIL THEPALE <trishathepale1016@gmail.com>
2026-06-23 19:55:10 +0530

    Create star_half_pyramid_.cpp
    ////star Pyramid triangle
    //Enter n: 5
    //    *
    //   * *
    //  * * *
    // * * * *
    //* * * * *

---
+++ Pattern Printing/star_half_pyramid_.cpp
@@ -0,0 +1,26 @@
+////star Pyramid triangle
+//Enter n: 5
+//    *
+//   * *
+//  * * *
+// * * * *
+//* * * * *
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"Enter n: ";
+	cin>>n;
+	
+	for(int i=1;i<=n;i++){
+		for(int j=1; j<=n-i;j++){
+			cout<<" ";
+		}
+		for(int j=1; j<=i; j++)
+		{
+			cout<<"* ";
+		}
+		cout<<endl;
+
+	}
+}
