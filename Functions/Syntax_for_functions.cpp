c6325ac567557bbd6d1c687684ec0a0f93341297
TRISHA ANIL THEPALE <trishathepale1016@gmail.com>
2026-06-23 20:12:45 +0530

    Create Syntax_for_functions.cpp
    // Functions block -> must be written before the int main( ){ }.

---
+++ Functions/Syntax_for_functions.cpp
@@ -0,0 +1,26 @@
+//FUNTIONS
+//output
+//Hello Good Morning
+//Hello Good Morning
+//This is india
+
+#include<iostream>
+using namespace std;
+
+//function block
+void greet(){
+	cout<<"Hello Good Morning"<<endl;
+	
+		return;
+		cout<<"Hello"<<endl;
+}
+
+void india(){
+		greet();
+	cout<<"This is india";
+}
+
+int main(){
+		greet();
+		india();	
+}
