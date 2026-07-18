dd1ac395495c54e844fe05c96479163ab8a6980d
TRISHA ANIL THEPALE <trishathepale1016@gmail.com>
2026-07-18 10:20:23 +0530

    Create array_of_num.cpp

---
+++ Array/array_of_num.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+//5 integers->6,2,8,5,0
+int arr[5];//declaration
+arr[0]=6;//set
+arr[1]=2;
+arr[2]=8;
+arr[3]=5;
+arr[4]=0;
+cout<<arr[0]<<" ";
+cout<<arr[1]<<" ";
+cout<<arr[2]<<" ";
+cout<<arr[3]<<" ";
+cout<<arr[4]<<" ";
+}
+//output:
+//6 2 8 5 0
