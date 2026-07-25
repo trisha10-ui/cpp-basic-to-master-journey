f1ce446de8bff08ca2f67d9aaa4a0b6ea91490dc
TRISHA ANIL THEPALE <trishathepale1016@gmail.com>
2026-07-25 16:08:21 +0530

    Create Concatination.cpp

---
+++ String/Concatination.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+	string s="Contact";
+	cout<<s<<s.length()<<endl;
+	s=s+"ination";
+//	s="ination"+s;
+	cout<<s<<s.length()<<endl;
+         
+}
+
+//output:Contact7
+//Contactination14
