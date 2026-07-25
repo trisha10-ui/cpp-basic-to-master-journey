6248dab10a9902b9dccba00e081cd2fd46b73b4c
TRISHA ANIL THEPALE <trishathepale1016@gmail.com>
2026-07-25 16:05:55 +0530

    Create clear_or_empty.cpp

---
+++ String/clear_or_empty.cpp
@@ -0,0 +1,16 @@
+//clear 
+#include<iostream>
+using namespace std;
+int main(){
+	string s="Clear";
+	cout<<s<<endl;
+	
+	cout<<s.length();
+	
+	s.clear();
+	cout<<s<<endl;
+}
+
+//Clear
+//5
+////space as it is clear
