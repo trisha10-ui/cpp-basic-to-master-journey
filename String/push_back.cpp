f1a4c938875e567819143e446c7dbccf110f6ac2
TRISHA ANIL THEPALE <trishathepale1016@gmail.com>
2026-07-25 16:09:20 +0530

    Create push_back.cpp

---
+++ String/push_back.cpp
@@ -0,0 +1,16 @@
+//push back 
+#include<iostream>
+using namespace std;
+int main(){
+	
+	string s="Push";
+	cout<<s<<endl;
+	
+	s.push_back('h');
+	cout<<s<<endl;
+	return 0;
+
+}
+
+//Push
+//Pushh
